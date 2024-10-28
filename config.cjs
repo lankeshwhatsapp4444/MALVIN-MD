0d51e74d540c67389f793e9ff5f89b7ec03095c7
lankeshwhatsapp4444 <151029357+lankeshwhatsapp4444@users.noreply.github.com>
2024-10-29 00:37:51 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -8,7 +8,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Bmwmd$W9EFnRQC",
+  SESSION_ID: process.env.SESSION_ID || "Bmwmd$uzgf7fZv",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
@@ -24,8 +24,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "✪⏤͟͞★⃝ꪶ‎𝙻𝙾𝚁𝙳 𝙼𝙰𝙻𝚅𝙸𝙽𖥘✪͜͡➺",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "263780166288",
+  OWNER_NAME: process.env.OWNER_NAME || "✪⏤͟͞★⃝ꪶ‎𝙻ankesh Sandaruwan𖥘✪͜͡➺",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94766498288",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
